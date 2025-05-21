4cf2a586dddd24cdbeaa74b847e8ce40a6ddef11
karimmustafa <karimmustafa495@gmail.com>
2025-05-21 20:36:42 +0300

    Create Login , signUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-
+import { CartComponent } from "./cart/cart.component";
+import { LoginComponent } from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CartComponent, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
