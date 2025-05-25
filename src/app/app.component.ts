b1ed77644ee0c2227bab4780ce6742066a40a432
karimmustafa <karimmustafa495@gmail.com>
2025-05-25 13:15:02 +0300

    Create About page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { CartComponent } from "./cart/cart.component";
 import { LoginComponent } from "./login/login.component";
-import {SignupComponent} from "./signup/signup.component";  
+import {SignupComponent} from "./signup/signup.component";
+import { AboutComponent } from "./about/about.component";  
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CartComponent, LoginComponent, SignupComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CartComponent, LoginComponent, SignupComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
