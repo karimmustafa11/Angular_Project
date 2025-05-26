8bd639f3bb3843d2a939fb1dbb57fb8a8d4f99c1
karimmustafa <karimmustafa495@gmail.com>
2025-05-26 21:41:43 +0300

    Create Women Fashion Section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,11 @@ import { AdsSliderComponent } from "../ads-slider/ads-slider.component";
 import { SportsSectionComponent } from "../sports-section/sports-section.component";
 import { OtherSectionComponent } from "../other-section/other-section.component";
 import { MenFashionComponent } from "../men-fashion/men-fashion.component";
+import { WomenFashionComponent } from "../women-fashion/women-fashion.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TreatYourselfTodayComponent, AdsSliderComponent, SportsSectionComponent, OtherSectionComponent, MenFashionComponent],
+  imports: [TreatYourselfTodayComponent, AdsSliderComponent, SportsSectionComponent, OtherSectionComponent, MenFashionComponent, WomenFashionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/women-fashion/women-fashion.component.ts
+++ src/app/women-fashion/women-fashion.component.ts
@@ -2,39 +2,40 @@ import { Component } from '@angular/core';
 import { CategorySectionComponent } from '../category-one-section/category-one-section.component';
 
 @Component({
-  selector: 'app-men-fashion',
-  templateUrl: './men-fashion.component.html',
+  selector: 'app-women-fashion',
   imports: [CategorySectionComponent],
+  templateUrl: './women-fashion.component.html',
+  styleUrl: './women-fashion.component.css',
 })
-export class MenFashionComponent {
+export class WomenFashionComponent {
   products = [
     {
-      image: 'images/men/top.png',
+      image: 'images/women/top.png',
       name: 'Tops & T-Shirts',
       discount: '30% OFF',
     },
     {
-      image: 'images/men/summer.png',
-      name: 'Summer Collection',
+      image: 'images/women/shirt.png',
+      name: 'Shirts & Blouses',
       discount: '20% OFF',
     },
     {
-      image: 'images/men/sport.png',
+      image: 'images/women/sport.png',
       name: 'Sportswear',
       discount: '25% OFF',
     },
     {
-      image: 'images/men/bag.png',
-      name: 'Bags',
+      image: 'images/women/bag.png',
+      name: 'Leather Bag',
       discount: '35% OFF',
     },
     {
-      image: 'images/men/foot.png',
+      image: 'images/women/foot.png',
       name: 'Footwear',
       discount: '20% OFF',
     },
     {
-      image: 'images/men/cap.png',
+      image: 'images/women/cap.png',
       name: 'Cap',
       discount: '15% OFF',
     }
