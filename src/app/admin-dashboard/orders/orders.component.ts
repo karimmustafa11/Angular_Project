6d678d99595e1fed8d689de22bb1ffd93aa7da1a
karimmustafa <karimmustafa495@gmail.com>
2025-06-13 18:34:52 +0300

    Handle orders in Dashboard

--- src/app/admin-dashboard/orders/orders.component.ts
+++ src/app/admin-dashboard/orders/orders.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  standalone: true,
+  imports: [CommonModule]
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/orders').subscribe(data => {
+      this.orders = data;
+      this.orders.reverse();
+    });
+  }
 }
